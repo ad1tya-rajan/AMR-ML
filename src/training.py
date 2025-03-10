f67371a089bd7e8ab1468dfa055a5e3a5aad3ef1
ad1tya-rajan <adityarajan53@gmail.com>
2025-03-10 16:07:04 -0500

    updated venv config, temporarily suppressed unresolved import warnings

--- src/training.py
+++ src/training.py
@@ -1,7 +1,7 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+import pandas as pd                                     # type: ignore  
+from sklearn.model_selection import train_test_split    # type: ignore
+from sklearn.ensemble import RandomForestClassifier     # type: ignore
+from sklearn.metrics import classification_report       # type: ignore
 
 from data_preprocessing import load_tsv_files, clean_data
 from feature_engineering import encode_features, create_target
